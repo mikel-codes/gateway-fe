8f306737009e943ab339a3004aa1bea0601f29a6
smeez <gatez@yahoo.com>
2022-05-02 23:53:13 +0100

    Fix: d.gateway is null

--- src/components/devices/Devices.js
+++ src/components/devices/Devices.js
@@ -81,12 +81,12 @@ const device = () => {
       {devices.length > 0 && devices.map((d,i) => <tr key={i}>
         <td><i className="fa-solid  fa-network-wired"></i></td>
 
-        <td>{d.gateway.name || "None"}</td>
-        <td>{d.gateway.ipv4 || "None"}</td>
+        <td>{d.gateway !== null && d.gateway.name || "None"}</td>
+        <td>{d.gateway !== null && d.gateway.ipv4 || "None"}</td>
         <td>{d.vendor}</td>
         <td>{d.uid == null ? "N/A" : d.uid}</td>
 
-        <td>{computeIP(d.gateway.ipv4, d.uid)}</td>
+        <td>{d.gateway !== null && computeIP(d.gateway.ipv4, d.uid) || "N/A"}</td>
         <td><span className={`switch ${d.status}`}>{d.status_name}</span></td>
         <td>{new Date(d.created_on).toLocaleString()}</td>
         <td>
