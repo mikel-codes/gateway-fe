9669319e8781f2de96a48101caa728c57e397b79
smeez <gatez@yahoo.com>
2022-05-02 11:54:45 +0100

    Fix: initialState not updating(#1)

--- src/components/gateway/Gateway.js
+++ src/components/gateway/Gateway.js
@@ -1,8 +1,12 @@
 import React, { useEffect, useState } from 'react'
-import {Link} from 'react-router-dom'
+import {Link, useNavigate} from 'react-router-dom'
 import {axiosApi} from '@@/api'
+import {deleteApi} from '@@/utils'
+
+
 const Gateway = () => {
   const [gateways, setGateways] = useState([])
+  const navigate = useNavigate()
   useEffect(() => {
     axiosApi.get("http://localhost:8000/api/router/gateways/").then(res => {
       if(res.status== 200){
@@ -11,6 +15,19 @@ const Gateway = () => {
       }
     }).catch(e => console.log(e))
   },[])
+  const removeGateway = async (gateway) => {
+    const confirm = window.confirm(`Do you want to remove this gateway with serial ${gateway.serial}? `)
+    if(confirm){
+      const res = await deleteApi(`gateways/${gateway.id}`)
+      if(res.status == 204){
+        alert("successfully deleted gateway")
+        setGateways(gateways => gateways = gateways.filter(g => g.id !== gateway.id))
+      }
+    }
+    return false
+
+  }
+
   return (
     <div className="tabs">
       <div style={{display: "flex", justifyContent: "space-around"}}>
@@ -18,7 +35,7 @@ const Gateway = () => {
     <input type="search" placeholder="search..." />
     </span>
     <span>
-      <Link className="link" to="new"><i className="fa-solid fa-plus"></i>Add Gateway</Link>
+      <button className="link" onClick={() => navigate('new')}><i className="fa-solid fa-plus"></i>Add Gateway</button>
     </span>
   </div>
     <table className="tabs">
@@ -34,19 +51,29 @@ const Gateway = () => {
       </thead>
       <tbody>
       {gateways.length > 0 && gateways.map((g,i) => <tr key={i}>
-         <td><i className="fa-solid fa-1.2x fa-microchip"></i></td>
+        <td><i className="fa-solid fa-1.2x fa-microchip"></i></td>
         <td>{g.serial || "FakeSerial"}</td>
         <td>{g.name}</td>
         <td>{g.ipv4}</td>
         <td style={{textAlign: "center"}}>{g.device_set.length}</td>
-        <td><i style={{color: "white"}} className="fa-solid fa-pen"></i></td>
-        <td><i style={{color: "blue"}} className="fa-solid fa-trash"></i></td>
-
-      </tr> || <tr><td colSpan={4}><center>No Gateways</center></td></tr>)}
-
+        <td>
+          <span className="btn" onClick={() => navigate("edit", {state: {gateway: g}})}>e</span>
+        </td>
+        <td onClick={() => removeGateway(g)}>
+          <i style={{color: "indianred"}} className="fa-solid fa-trash"></i>
+        </td>
+      </tr>)
+      ||
+      <tr>
+        <td colSpan={6}>
+          <center>No Gateways</center>
+        </td>
+      </tr>}
       </tbody>
     </table>
     </div>
+
+
   )
 }
 
--- src/components/gateway/GatewayForm.js
+++ src/components/gateway/GatewayForm.js
@@ -1,52 +1,71 @@
 import React, {useState} from 'react'
+import {useLocation} from "react-router-dom"
 import {FormValidator, ValidationMessage} from '@@/forms'
-import {axiosApi} from '@@/api'
+import {createApi, updateApi} from "@@/utils"
 
-let initialState = {
-  serial: "",
-  name: "",
-  ipv4: "",
-}
 
-let gatewayRules =  {
-  serial: { required:true, minlength: 5, maxlength: 12, numeric: true },
-  name: { required: true, minlength: 5, maxlength: 10, alpha: true },
-  ipv4: { required: true, ipv4: true }
-}
 const GatewayForm = () => {
+  let initialState = {
+    serial: "",
+    name: "",
+    ipv4: "",
+  }
+  let gatewayRules =  {
+    serial: { required:true, minlength: 5, maxlength: 12, numeric: true },
+    name: { required: true, minlength: 5, maxlength: 10, alpha: true },
+    ipv4: { required: true, ipv4: true }
+  }
+  let gw = null
+  const loc =  useLocation()
+
+  if(loc.state !== null){
+    const {gateway} = loc.state
+    gw = gateway
+    initialState = {
+      serial: gw.serial,
+      name: gw.name,
+      ipv4: gw.ipv4
+    }
+  }
+
   const [payload, setPayload] = useState(initialState)
   const [rules, setRules] = useState(gatewayRules)
 
-  const handleSubmit = (payload) => {
-    axiosApi.post("gateways/", payload).then(res => {
-      if(res.status== 201)
-        alert("created a new gateway")
-    }).catch(e => console.log(e))
+  const handleSubmit = async (payload) => {
+    let resp = null
+    if(gw == null)
+      resp =await createApi("gateways/", payload)
+    else
+      resp = await updateApi(`gateways/${gw.id}/`, payload)
+    if(resp.status == 201)
+      alert("created successfully")
+    if(resp.status == 200)
+      alert("updated successfully")
+
+
   }
 
   return (
-    <FormValidator data={payload} rules={rules} submit={handleSubmit}>
+    <FormValidator data={payload} rules={rules} submit={handleSubmit} isEdit={loc.state}>
       <div className="form">
         <div className="form_field">
           <label htmlFor="serial">Serial</label>
-          <input name="serial" default={payload.serial} onChange={e => setPayload({...payload, serial: e.target.value}) }/>
+          <input name="serial" defaultValue={payload.serial} onChange={e => setPayload({...payload, serial: e.target.value}) }/>
            <ValidationMessage field="serial" />
         </div>
         <div className="form_field">
           <label htmlFor="name">Name</label>
 
-          <input name="name" default={payload.name} onChange={e => setPayload({...payload, name: e.target.value}) } />
+          <input name="name" defaultValue={payload.name} onChange={e => setPayload({...payload, name: e.target.value}) } />
             <ValidationMessage field="name" />
         </div>
 
         <div className="form_field">
           <label htmlFor="ipv4">IPv4</label>
-          <input name="ipv4" name="ipv4" default={payload.ipv4} onChange={e => setPayload({...payload, ipv4: e.target.value}) } />
+          <input name="ipv4" name="ipv4" defaultValue={payload.ipv4} onChange={e => setPayload({...payload, ipv4: e.target.value}) } />
           <ValidationMessage field="ipv4" />
         </div>
-
       </div>
-
   </FormValidator>
   )
 }
