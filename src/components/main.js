65327c509104967282c0616614c90e999cbdf4ad
smeez <gatez@yahoo.com>
2022-05-02 23:08:12 +0100

    Added ErrorBoundary and React Suspense

--- src/components/main.js
+++ src/components/main.js
@@ -1,13 +1,18 @@
-import React from 'react'
+import React , {Suspense} from 'react'
 import {Outlet} from 'react-router-dom'
 import Side from "./Side"
+import ErrorBoundary from './ErrorBoundary'
 
 const Main = (props) => {
   return (
     <>
     <Side />
     <main className="wrap-all-the-things">
-      <Outlet />
+      <ErrorBoundary>
+        <Suspense fallback={<div className="loader"></div>}>
+          <Outlet />
+      </Suspense>
+      </ErrorBoundary>
     </main>
     </>
   )
