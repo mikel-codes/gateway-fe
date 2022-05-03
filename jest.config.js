3971d18afb673f8157749c15405dddbb6f4d1e81
smeez <gatez@yahoo.com>
2022-05-03 14:30:04 +0100

    Added passing test for App.js

--- jest.config.js
+++ jest.config.js
@@ -1,9 +1,16 @@
-const path = require("path");
-
+const {defaults} = require('jest-config');
 module.exports = {
+    "testEnvironment": "jsdom",
    moduleNameMapper: {
     "^@/(.*)$": "<rootDir>/src/$1",
     "^@@/(.*)$": "<rootDir>/src/components/$1"
 
-    }
+  },
+
 }
+/*
+setupFilesAfterEnv: [
+  '@testing-library/react/cleanup-after-each',
+  '@testing-library/jest-dom/extend-expect'
+],
+*/
--- src/App.test.js
+++ src/App.test.js
@@ -1,12 +1,13 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
+import { BrowserRouter as BR } from 'react-router-dom';
 import { createRoot } from 'react-dom/client';
 
 import App from './App';
 
 it('renders without crashing', () => {
-  const container = document.getElementById('app');
-  const root = createRoot(container);
-  root.render(<App />, container);
-  ReactDOM.unmountComponentAtNode(container);
+  const  div = document.createElement('div');
+  const root = createRoot(div)
+  root.render(<BR><App /></BR>);
+  root.unmount(div);
 });
