a9e1428c46fe9fa3eba69528519fb8d61563b59b
smeez <gatez@yahoo.com>
2022-05-04 19:18:51 +0100

    Fix: implemented navigate hook

--- src/components/NotFound.js
+++ src/components/NotFound.js
@@ -2,10 +2,11 @@ import React from 'react'
 import {useNavigate} from 'react-router-dom'
 
 const NotFound = () => {
+  let navigate = useNavigate()
   return (
     <div className="tabs">
       <h1>Page You Seek Does Not Exist</h1>
-      <button onClick={() => navigate('gateways')}>Go Back</button>
+      <button onClick={() => navigate('gateways')}>Go to Main Page</button>
     </div>
   )
 }
