6fb41cc1fc347799933be887a9a58bc2fc1645e2
smeez <gatez@yahoo.com>
2022-05-06 21:36:14 +0100

    Set baseURL to localhost

--- src/components/api/axiosApi.js
+++ src/components/api/axiosApi.js
@@ -2,8 +2,8 @@ import React from 'react';
 import Axios from 'axios';
 
 const axiosApi = Axios.create({
-  //baseURL: "http://localhost:8000/api/router/",
-  baseURL: "https://gwapii.herokuapp.com/api/router",
+  baseURL: "http://localhost:8000/api/router/",
+  //baseURL: "https://gwapii.herokuapp.com/api/router",
   timeout: 3000,
 })
 export default axiosApi
